dcaa1ead1e0b50f769d54e6082e8a5526e0b2c8b
JumaKahiga <kabirumwangi@gmail.com>
2018-11-08 22:59:01 +0300

    [third commit #161771811]

--- app/api/tests/test_base.py
+++ app/api/tests/test_base.py
@@ -3,12 +3,21 @@ from app import create_app
 from app.api.v1.models import ParcelOrder
 
 parcels = ParcelOrder()
+parcel_dummy_data= {"parcel_id": "100", 
+					"client_name": "John Doe", 
+					"package_desc": "Television", 
+					"location": "Mirema", 
+					"destination": "Buruburu", 
+					"pickup_date": "12 November 2018", 
+					"parcel_status": "Delivered"}
 
 
 class BaseTest(unittest.TestCase):
-	"""Parent Test Class"""
-   def setUp(self):
-       self.app = create_app()
-       self.client = self.app.test_client()
-       self.test_parcel= {"name":"username","phonenumber":"5678954321","idnumber":"3456890","location":"nairobi","address":"3467134","weight":"2"}    def tearDown(self):
-       parcels.db.clear()
+	
+	def setUp(self):
+		self.app = create_app()
+        self.client = self.app.test_client()
+        self.test_parcel= parcel_dummy_data
+
+    def tearDown(self):
+    	parcels.db.clear()
