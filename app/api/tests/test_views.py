6e986cebbadf59581aa7da00b8c67c57567724a6
JumaKahiga <kabirumwangi@gmail.com>
2018-11-09 19:13:15 +0300

    [second commit #161840264]

--- app/api/tests/test_views.py
+++ app/api/tests/test_views.py
@@ -1,17 +1,27 @@
 import unittest
 import json
-from test_base import BaseTest
+from app.api.tests.test_base import BaseTest
 
+
+parcel_dummy_data= {"parcel_id": "100", 
+					"client_name": "John Doe", 
+					"package_desc": "Television", 
+					"location": "Mirema", 
+					"destination": "Buruburu", 
+					"pickup_date": "12 November 2018", 
+					"parcel_status": "Delivered"}
 
 class TestParcel(BaseTest):
 	"""docstring for TestParcel"""
 	def test_new_parcel(self):
-		respo= self.client.post('/api/v1/parcels',data = json.dumps(self.test_parcel), content_type='application/json')
-		self.assertEqual(resp.status_code,200)
+		respo= self.client.post('/api/v1/parcel',data = json.dumps(parcel_dummy_data), content_type='application/json')
+		self.assertEqual(respo.status_code,200)
 
 
 	def test_single_parcel(self):
 		respo= self.client.get('/api/v1/parcels')
 		self.assertEqual(respo.status_code, 200)
 
-		
+
+if __name__ == "__main__":
+	unittest.main()
--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,6 +1,35 @@
 users= []
 
+# User roles
+customer = "Customer"
+site_admin = "Admin"
 
+
+class UserModel(object):
+    """Creating user model"""
+    def __init__(self):
+        self.udb = users
+        self.user_id = len(self.udb)
+        self.user_role = customer
+
+    def new_user(self, name, email, password, contact_phone):
+        user_data = {
+            "user_id": self.user_id + 1,
+            "name": name,
+            "email": email,
+            "password": password,
+            "contact_phone": contact_phone,
+        }
+
+        created_user = self.udb.append(user_data)
+        return created_user
+
+    def single_user(self, user_id):
+        for user in users:
+            if user["user_id"] == user_id:
+                return user
+            else:
+                return {'User': 'Not Available'}, 404
 
 
 parcels = []
@@ -33,7 +62,27 @@ class ParcelOrder(object):
         order= self.db.append(new_order_data)
         return order
 
+    def parcels_list(self):
+        return self.db
 
+    def single_parcel(self, parcel_id):
+        for parcel in parcels:
+            if parcel["parcel_id"]== parcel_id:
+                return parcel
+            else:
+                return {'parcel': 'Not Available'}, 404
 
+    def cancel_order(self, parcel_id):
+        for parcel in parcels:
+            if parcel["status"] == delivered:
+                return {'parcel': "Cannot be cancelled. Already delivered"}
+            elif parcel['parcel_id'] == parcel_id:
+                parcel.update({"status": cancelled})
+                return {'parcel': 'Order Cancelled'}
+
+    def specific_user_orders(self, user_id):
+        pass
+    def clear(self):
+    	self.db = []
 
 
