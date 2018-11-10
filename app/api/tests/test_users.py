8de086b0a6c087d9933a1b00c5af879810d119f5
JumaKahiga <kabirumwangi@gmail.com>
2018-11-10 13:21:20 +0300

    [fixes tests for #161858721]

--- app/api/tests/test_users.py
+++ app/api/tests/test_users.py
@@ -6,16 +6,12 @@ from app.api.tests.test_base import BaseTest
 class TestUser(BaseTest):
 	"""docstring for TestUser"""
 	def test_new_user(self):
-		respo= self.client.post('/api/v1/parcel',data = json.dumps(self.sample_parcel), content_type='application/json')
-		self.assertEqual(respo.status_code,200)
+		respo= self.client.post('/api/v1/users',data = json.dumps(self.sample_user), content_type='application/json')
+		self.assertEqual(respo.status_code, 201)
 
-	def test_all_parcels(self):
-		respo= self.client.get('/api/v1/parcels')
-		self.assertEqual(respo.status_code, 200)
-
-	def test_single_parcel(self):
-		parcel_id= self.new_parcel_id
-		respo= self.client.get('/api/v1/parcels/' + parcel_id)
+	def test_single_user(self):
+		user_id= self.new_user_id
+		respo= self.client.get('/api/v1/users/' + user_id)
 		self.assertEqual(respo.status_code, 200)
 
 
